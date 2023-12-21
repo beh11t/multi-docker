df3ea1be2614dca90906a9595424489dd37d16a1
Behnam <behnam.maleki@mport.com.au>
2023-12-21 17:09:33 +1100

    pg client update

--- server/index.js
+++ server/index.js
@@ -11,16 +11,24 @@ app.use(bodyParser.json());
 
 // Postgres Client Setup
 const { Pool } = require('pg');
+// const pgClient = new Pool({
+//   user: keys.pgUser,
+//   host: keys.pgHost,
+//   database: keys.pgDatabase,
+//   password: keys.pgPassword,
+//   port: keys.pgPort,
+//   ssl:
+//     process.env.NODE_ENV !== 'production'
+//       ? false
+//       : { rejectUnauthorized: false },
+// });
 const pgClient = new Pool({
   user: keys.pgUser,
   host: keys.pgHost,
   database: keys.pgDatabase,
   password: keys.pgPassword,
   port: keys.pgPort,
-  ssl:
-    process.env.NODE_ENV !== 'production'
-      ? false
-      : { rejectUnauthorized: false },
+  ssl: { rejectUnauthorized: false },
 });
 
 pgClient.on('connect', (client) => {
@@ -28,6 +36,7 @@ pgClient.on('connect', (client) => {
     .query('CREATE TABLE IF NOT EXISTS values (number INT)')
     .catch((err) => console.error(err));
 });
+
 
 // Redis Client Setup
 const redis = require('redis');
