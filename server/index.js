cd58c8e49fc9091a8ded3d8d642d2d2ab41f2dfc
Behnam <behnam.maleki@mport.com.au>
2023-12-21 17:23:03 +1100

    another update

--- server/index.js
+++ server/index.js
@@ -11,25 +11,25 @@ app.use(bodyParser.json());
 
 // Postgres Client Setup
 const { Pool } = require('pg');
-// const pgClient = new Pool({
-//   user: keys.pgUser,
-//   host: keys.pgHost,
-//   database: keys.pgDatabase,
-//   password: keys.pgPassword,
-//   port: keys.pgPort,
-//   ssl:
-//     process.env.NODE_ENV !== 'production'
-//       ? false
-//       : { rejectUnauthorized: false },
-// });
 const pgClient = new Pool({
   user: keys.pgUser,
   host: keys.pgHost,
   database: keys.pgDatabase,
   password: keys.pgPassword,
   port: keys.pgPort,
-  ssl: { rejectUnauthorized: false },
+  // ssl:
+  //   process.env.NODE_ENV !== 'production'
+  //     ? false
+  //     : { rejectUnauthorized: false },
 });
+// const pgClient = new Pool({
+//   user: keys.pgUser,
+//   host: keys.pgHost,
+//   database: keys.pgDatabase,
+//   password: keys.pgPassword,
+//   port: keys.pgPort,
+//   ssl: { rejectUnauthorized: false },
+// });
 
 pgClient.on('connect', (client) => {
   client
